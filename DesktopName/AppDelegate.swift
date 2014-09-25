55899d85fe84aff3d3a213ac70b087bd2f41744a
Stephen Sykes <sdsykes@gmail.com>
2014-09-25 14:06:28 +0300

    Initial Commit

---
+++ DesktopName/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DesktopName
+//
+//  Created by Stephen Sykes on 25/09/14.
+//  Copyright (c) 2014 Switchstep. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DesktopName/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  DesktopName
+//
+//  Created by Stephen Sykes on 25/09/14.
+//  Copyright (c) 2014 Switchstep. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
